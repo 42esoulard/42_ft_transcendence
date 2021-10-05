ed7ebfd9902c296d6290e1e47fc5eb45ee1711b0
rturcey <rturcey@student.42.fr>
2021-10-05 10:00:52 +0200

    add sass, front architecture, sidebar, header

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import api from "@/plugins/api.plugin";
+import '@fortawesome/fontawesome-free/js/all'
 
 // Config for the sdk (not used anymore)
 const config = {
@@ -10,4 +11,4 @@ const config = {
 
 createApp(App).use(router)
   // .use(api, config)
-  .mount('#app')
+  .mount('.app')
