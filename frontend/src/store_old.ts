db4ffe5e3ef5249640f9e7591bd52db7a58cb159
Sequoiageant <j.nolleau88@gmail.com>
2021-10-07 17:40:57 +0200

    delete store_old.ts

--- frontend/src/store_old.ts
+++
@@ -1,9 +0,0 @@
-import { reactive, computed } from 'vue'
-import { User } from './types/User'
-// state
-const state = reactive({
-  user: null as User | null
-})
-export default () => ({
-  user: computed(() => state.user)
-})
