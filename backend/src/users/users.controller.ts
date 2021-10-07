15a700165f453fb5bd4eb8d718248d9ae20b94e6
Sequoiageant <j.nolleau88@gmail.com>
2021-10-07 20:30:23 +0200

    add POST uploaded image endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException, UseInterceptors, UploadedFile, Res, StreamableFile } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './interfaces/user.interface';
 import { CreateUserDto } from './dto/createUser.dto';
 import { ApiBadRequestResponse, ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
-// import { UpdateUserDto } from './dto/updateUser.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName, imageFileFilter } from '../utils/files-upload.utils';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -20,6 +25,29 @@ export class UsersController {
 		}
 		return users;
 	}
+
+	/**
+	 * Return an avatar from its path
+	 */
+	@Get('avatar/:imgpath')
+	getAvatar(
+		@Param('imgpath') path: string,
+		@Res({ passthrough: true }) res: Response
+		): StreamableFile {
+		const file = createReadStream(join('./uploads/', path));
+		res.set({
+			'Content-Type': 'image/png',
+			'Content-Disposition': 'attachment; filename="avatar.png"',
+		});
+		return new StreamableFile(file);
+	}
+
+
+		// @Get(':imgpath')
+	// seeUploadedFile(@Param('imgpath') image, @Res() res: Response) {
+	// 	return res.sendFile(image, { root: './files' });
+	// }
+
 
 	/**
 	* Returns a user found in database by its id.
@@ -42,6 +70,8 @@ export class UsersController {
 		}
 		return user;
 	}
+
+
 
 	/**
 	* Returns a user found in database by its username.
@@ -72,6 +102,29 @@ export class UsersController {
 	async saveUser(@Body() newUser: CreateUserDto): Promise<User> {
 		return await this.userService.saveUser(newUser);
 	}
+
+
+	/**
+	 * Recieve user picture from frontend
+	 * @param file picture
+	 */
+	@Post('upload')
+	@UseInterceptors(FileInterceptor('avatar', {
+		storage: diskStorage({
+			destination: './uploads',
+			filename: editFileName,
+		}),
+		fileFilter: imageFileFilter,
+	}))
+	uploadFile(@UploadedFile() file: Express.Multer.File) {
+		console.log(file);
+		const response = {
+			originalname: file.originalname,
+			filename: file.filename,
+		};
+		return response;
+	}
+
 
 	// @Post()
 	// @ApiCreatedResponse({
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,9 +3,15 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entity/users.entity';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    MulterModule.register({
+      dest: './uploads',
+    })
+  ],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService]
