167426dbc732e5881c3f2be330ed58647934f4cc
grim <grim@grim.fr>
2021-10-20 12:14:28 +0200

    draw net

--- frontend/src/composables/draw.js
+++ frontend/src/composables/draw.js
@@ -4,6 +4,9 @@ import { ref } from 'vue'
 var BALL_RADIUS = 10
 var RACQUET_LENGTH = 80
 var RACQUET_WIDTH = 20
+var NET_ELEM_LENGHT = 40
+var NET_ELEM_GAP = 10
+var NET_WIDTH = 5
 
 const getDraw = () => {
 
@@ -18,15 +21,28 @@ const getDraw = () => {
 
 		context.value.beginPath()
 
+		// racquets
 		context.value.rect(0, playerPositions.value.player1, RACQUET_WIDTH, RACQUET_LENGTH)
 		context.value.rect(game.value.width - RACQUET_WIDTH, playerPositions.value.player2, RACQUET_WIDTH, RACQUET_LENGTH)
+		// ball
 		context.value.arc(ballPosition.value.x, ballPosition.value.y, BALL_RADIUS, 0, Math.PI*2, false);
+		
+
+		context.value.fill()
+		context.value.closePath()
+		
+		//scores
 		context.value.font = "80px Arial";
 		context.value.fillText(score.value.player1, game.value.width / 4, game.value.height / 5)
 		context.value.fillText(score.value.player2, game.value.width * 3 / 4, game.value.height / 5)
 		
-		context.value.fill()
-		context.value.closePath()
+		// net
+		context.value.beginPath()
+		context.value.lineWidth = NET_WIDTH
+		context.value.setLineDash([NET_ELEM_LENGHT, NET_ELEM_GAP])
+		context.value.moveTo(game.value.width / 2, 0);
+		context.value.lineTo(game.value.width /2, game.value.height);
+		context.value.stroke()
 	}
 
 	return { context, game, ballPosition, playerPositions, score, draw}
