743648fa49f3f97637a8e05bec0dffa14da43d6f
grim <bbrunet@student.42.fr>
2021-11-26 16:24:43 +0100

    error corrected: wrong socket from player2

--- backend/src/pong/pong.gateway.ts
+++ backend/src/pong/pong.gateway.ts
@@ -118,7 +118,7 @@ export class PongGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     }
     // this.logger.log('challenge accepted ', message)
     const player1 = new player(challenge.challengerId, challenge.challengeeName, client, 1)
-    const player2 = new player(challenge.challengerId, challenge.challengerName, client, 2)
+    const player2 = new player(challenge.challengerId, challenge.challengerName, challenge.challengerSocket, 2)
     this.createGame(player1, player2, challenge.gameMode)
   }
   
