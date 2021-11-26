1a78ec677587ea019d28812b5c66f2da365312a2
jnolleau <j.nolleau88@gmail.com>
2021-11-26 14:24:35 +0100

    add pong notification - needs improvement

--- frontend/src/components/SideBarLink.vue
+++ frontend/src/components/SideBarLink.vue
@@ -1,6 +1,7 @@
 <template>
   <router-link :to="to" class="sidebar-link" :class="{ active: isActive }">
     <i class="sidebar-link__icon" :class="icon" />
+    <div v-if="hasNotification" class="sidebar-link__notif"></div>
     <slot />
   </router-link>
 </template>
@@ -8,6 +9,7 @@
 <script lang="ts">
 import { computed } from "vue";
 import { useRoute } from "vue-router";
+import { store } from "@/store";
 
 export default {
   props: {
@@ -17,7 +19,34 @@ export default {
   setup(props: any) {
     const route = useRoute();
     const isActive = computed(() => route.path === props.to);
-    return { isActive };
+    // const hasChallenges = computed(
+    //   () =>
+    //     store.state.onlineUsers.length > 1 &&
+    //     props.to === "/pong" && // test à faire sur le tableau de challenges
+    //     !isActive.value
+    // );
+    // const hasLives = computed(
+    //   () =>
+    //     store.state.inGameUsers.length > 0 &&
+    //     props.to === "/pong/watch" &&
+    //     !isActive.value
+    // );
+    const hasNotification = computed(() => 
+      ((store.state.inGameUsers.length > 0 && props.to === "/pong/watch")
+      || (store.state.onlineUsers.length > 1 && props.to === "/pong")) // test à faire sur le tableau de challenges
+      && !isActive.value);
+
+    // console.log("inGameUsers", store.state.inGameUsers);
+    // console.log("hasLives", hasLives.value);
+
+    return {
+      isActive,
+      hasNotification,
+      // hasChallenges,
+      // hasLives,
+      // challenges: computed(() => store.state.onlineUsers.length),
+      // liveGames: computed(() => store.state.inGameUsers.length / 2),
+    };
   },
 };
 </script>
