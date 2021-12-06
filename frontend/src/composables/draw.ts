c0d66ed20a08d8d91c163c76c29fd8cac0e0b953
Basile BRUNET <bbrunet@e1r9p7.42.fr>
2021-12-06 14:21:55 +0100

    fixed: VUE APP RACQUET LENGTH TRANSCENDENCE was not equal to RACQUET LENGTH TRANSCENDENCE -- previous commit useless

--- frontend/src/composables/draw.ts
+++ frontend/src/composables/draw.ts
@@ -91,7 +91,6 @@ const getDraw = (mode: gameMode) => {
     if (mode == "transcendence") {
       racquetLenghtRatio.value.player1 = RACQUET_LENGTH_RATIO_TRANSCENDENCE;
       racquetLenghtRatio.value.player2 = RACQUET_LENGTH_RATIO_TRANSCENDENCE;
-      console.log(RACQUET_LENGTH_RATIO_TRANSCENDENCE)
     }
   };
 
